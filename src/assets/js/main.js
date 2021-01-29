58ba371e201b071cbd9ebd5e8332c5ec9d1aaed5
serhatbek <serhat.bekk@gmail.com>
2021-01-29 20:40:22 +0300

    Form Validation with PristineJS

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -12,3 +12,56 @@ openBurgerNav.addEventListener('click', () => {
     headerNav.classList.add('open');
   }
 });
+
+// SIGNIN SIGNUP MODALS
+const signinModalBtn = document.querySelector('.sign-btns__login');
+const signupModalBtn = document.querySelector('.sign-btns__signup');
+const closeModalBtn = document.querySelector('.close-modal-btn');
+
+const modal = document.querySelector('.modal');
+
+const loginForm = document.querySelector('.signin-form');
+const signupForm = document.querySelector('.signup-form');
+
+signinModalBtn.addEventListener('click', () => openLoginModal());
+signupModalBtn.addEventListener('click', () => openSignupModal());
+closeModalBtn.addEventListener('click', () => closeModal());
+
+function openLoginModal() {
+  modal.classList.add('open');
+  loginForm.classList.add('open');
+}
+
+function openSignupModal() {
+  modal.classList.add('open');
+  signupForm.classList.add('open');
+}
+
+function closeModal() {
+  modal.classList.remove('open');
+  loginForm.classList.remove('open');
+  signupForm.classList.remove('open');
+}
+
+// FORM VALIDATION
+
+var pristine;
+window.onload = function () {
+  var formLog = document.getElementById('form1');
+  var form = document.getElementById('form2');
+
+  pristine = new Pristine(form);
+  pristine = new Pristine(formLog);
+
+  form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    var valid = pristine.validate();
+    alert('Form is valid: ' + valid);
+  });
+
+  formLog.addEventListener('submit', function (e) {
+    e.preventDefault();
+    var valid = pristine.validate();
+    alert('Form is valid: ' + valid);
+  });
+};
